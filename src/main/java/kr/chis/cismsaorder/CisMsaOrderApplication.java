b37d4d720b01771af3c226a233e72f934879adce
CIS-Mac_BW <kr.chis@gmail.com>
2020-07-15 08:46:09 +0900

    first commit

---
+++ src/main/java/kr/chis/cismsaorder/CisMsaOrderApplication.java
@@ -0,0 +1,13 @@
+package kr.chis.cismsaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisMsaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CisMsaOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/chis/cismsaorder/CisMsaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.chis.cismsaorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CisMsaOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
